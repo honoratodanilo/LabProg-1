dd93887da33c5cf45f1c1c8fbd48385187f03081
Ivan Simplício <ivansimplicio@outlook.com>
2017-09-19 14:04:57 -0300

    Lab02 - Jogo implementado.

---
+++ atividades/Lab01/src/principal/MinMaxRefatorado.java
@@ -0,0 +1,13 @@
+package principal;
+
+/*
+ * Aluno: Ivanildo Simplício da Silva Filho
+ * Atividade 01 - Laboratório de Programação
+ */
+
+public class MinMaxRefatorado {
+
+	public static void main(String[] args) {
+		
+	}
+}
