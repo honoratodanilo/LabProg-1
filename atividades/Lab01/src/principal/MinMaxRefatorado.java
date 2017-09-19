1b27c2c5af19a275f92e42d1a50c0f9e94424c72
Ivan Simplício <ivansimplicio@outlook.com>
2017-09-19 15:25:09 -0300

    Lab01 - MinMaxRefatorado.

--- atividades/Lab01/src/principal/MinMaxRefatorado.java
+++ atividades/Lab01/src/principal/MinMaxRefatorado.java
@@ -1,5 +1,7 @@
 package principal;
 
+import java.util.Scanner;
+
 /*
  * Aluno: Ivanildo Simplício da Silva Filho
  * 
@@ -8,6 +10,25 @@ package principal;
 public class MinMaxRefatorado {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int valores[] = new int[3];
+		int min, max;
 		
+		for(int i = 0; i < valores.length; i++) {
+			System.out.printf("Insira o valor %d: ", i+1);
+			valores[i] = sc.nextInt();
+		}
+		
+		min = max = valores[0];
+		for(int i = 1; i < valores.length; i++) {
+			if(valores[i] > max) {
+				max = valores[i];
+			}else if(valores[i] < min){
+				min = valores[i];
+			}
+		}
+		
+		System.out.println("Menor valor: "+min);
+		System.out.println("Maior valor: "+max);
 	}
 }
